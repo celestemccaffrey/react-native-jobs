ab9c97f64ec5de8e9be853e66ae3cdbefdcb307e
Celeste McCaffrey <c_mccaffrey@outlook.com>
2023-12-29 15:22:01 -0500

    Added header buttons

--- app/index.js
+++ app/index.js
@@ -1,11 +1,35 @@
-import { View, Text } from 'react-native';
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
-    return (
-        <View>
-        <Text>Home</Text>
-        </View>
-    );
-}
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.search} dimension="60%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+    </SafeAreaView>
+  );
+};
 
 export default Home;
