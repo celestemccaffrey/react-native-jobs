ff8682163825f8634c92cfdc4e60bd68055e35f1
Celeste McCaffrey <cmccaffrey@civiconnect.ca>
2024-03-01 01:50:08 -0500

    Minor changes

---
+++ app/job-details/[id].js
@@ -0,0 +1,16 @@
+search.js
+import React, { useEffect, useState } from 'react'
+import { ActivityIndicator, FlatList, Image, TouchableOpacity, View } from 'react-native'
+import { Stack, useRouter, useSearchParams } from 'expo-router'
+import { Text, SafeAreaView } from 'react-native'
+import axios from 'axios'
+
+import { ScreenHeaderBtn, NearbyJobCard } from '../../components'
+import { COLORS, icons, SIZES } from '../../constants'
+import styles from '../../styles/search'
+
+const JobDetails = () => {
+  return <Text>JobDetails</Text>;
+};
+
+export default JobDetails;
