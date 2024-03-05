427d055bef01acbd88491fbb7e44a0274a9de16e
Celeste McCaffrey <cmccaffrey@civiconnect.ca>
2024-03-05 09:04:29 -0500

    Minor change

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,15 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import { ActivityIndicator, FlatList, Image, TouchableOpacity, View } from 'react-native'
-import { Stack, useRouter, useSearchParams } from 'expo-router'
-import { Text, SafeAreaView } from 'react-native'
-import axios from 'axios'
+import {
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import { useCallback, useState } from "react";
 
-import { ScreenHeaderBtn, NearbyJobCard } from '../../components'
-import { COLORS, icons, SIZES } from '../../constants'
-import styles from '../../styles/search'
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+  Specifics,
+} from "../../components/job-details";
+import { COLORS, icons, SIZES } from "../../constants";
+
+import useFetch from "../../hooks/useFetch";
+import styles from "../../styles/search";
 
 const JobDetails = () => {
-  return <Text>JobDetails</Text>;
+  const params = useSearchParams();
+  const router = useRouter();
+
+  const { data, isLoading, error, refetch } = useFetch("job-details", {
+    job_id: params.id,
+  });
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+      options={{
+        headerStyle: {backgroundColor: COLORS.lightWhite},
+      }}>
+
+      </Stack.Screen>
+    </SafeAreaView> 
+  ) 
 };
 
 export default JobDetails;
