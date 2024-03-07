460a6d4795fb3e4da0f618a18617e4d0f05dbefd
Celeste McCaffrey <cmccaffrey@civiconnect.ca>
2024-03-07 08:02:01 -0500

    Minor commit

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -22,29 +22,45 @@ import { COLORS, icons, SIZES } from "../../constants";
 
 import useFetch from "../../hook/useFetch";
 
-import React from 'react'
+import React from "react";
 
 const JobDetails = () => {
   const params = useGlobalSearchParams();
   const router = useRouter();
 
   const { data, isLoading, error, refetch } = useFetch("job-details", {
-    job_id: params.id
+    job_id: params.id,
   });
+
+  const [refreshing, setRefreshing] = useState(false);
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
-      options={{
-        headerStyle: {backgroundColor: COLORS.lightWhite},
-        // headerShadowVisible: false, // Removes the header shadow
-        // headerBackVisible: false, // Removes the back button
-
-      }}>
-
-      </Stack.Screen>
-    </SafeAreaView> 
-  ) 
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false, // Removes the header shadow
+          headerBackVisible: false, // Removes the back button
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimension="60%"
+              handlePress={() => router.back()}
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.share} dimension="60%" />
+          ),
+          headerTitle: ''
+        }}
+      />
+      <>
+      {/* <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+        
+      </ScrollView> */}
+      </>
+    </SafeAreaView>
+  );
 };
 
-export default JobDetails
+export default JobDetails;
